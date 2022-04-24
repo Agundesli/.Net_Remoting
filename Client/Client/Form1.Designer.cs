c0da01e78eb43945c38a7af412a5bdccf00d9ba8
Abdullah <agundesli06@gmail.com>
2022-04-24 21:44:31 +0300

    Client

--- Client/Client/Form1.Designer.cs
+++ Client/Client/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.AnaMetin = new System.Windows.Forms.TextBox();
+            this.IslemSonucu = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "KucukHarf";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(336, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 54);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "BuyukHarf";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(503, 53);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 51);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "TersCevir";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // AnaMetin
+            // 
+            this.AnaMetin.Location = new System.Drawing.Point(76, 169);
+            this.AnaMetin.Multiline = true;
+            this.AnaMetin.Name = "AnaMetin";
+            this.AnaMetin.Size = new System.Drawing.Size(258, 93);
+            this.AnaMetin.TabIndex = 4;
+            // 
+            // IslemSonucu
+            // 
+            this.IslemSonucu.Location = new System.Drawing.Point(373, 169);
+            this.IslemSonucu.Multiline = true;
+            this.IslemSonucu.Name = "IslemSonucu";
+            this.IslemSonucu.Size = new System.Drawing.Size(242, 93);
+            this.IslemSonucu.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.IslemSonucu);
+            this.Controls.Add(this.AnaMetin);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox AnaMetin;
+        private System.Windows.Forms.TextBox IslemSonucu;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace Client
@@ -15,6 +9,25 @@ namespace Client
         public Form1()
         {
             InitializeComponent();
+        }
+        UzakNesne.Nesne nesne;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nesne = Activator.GetObject(typeof(UzakNesne.Nesne), "tcp://localhost:1000/AdSoyad") as UzakNesne.Nesne;                                                           
+            nesne = Activator.GetObject(typeof(UzakNesne.Nesne), "tcp://192.168.1.223:1000/KucukHarf") as UzakNesne.Nesne;                                                        
+            nesne = Activator.GetObject(typeof(UzakNesne.Nesne), "tcp://192.168.1.223:1001/BuyukHarf") as UzakNesne.Nesne;                                                        
+            nesne = Activator.GetObject(typeof(UzakNesne.Nesne), "tcp://192.168.1.223:1000/TersCevir") as UzakNesne.Nesne;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IslemSonucu.Text = nesne.KucukHarfCevir(AnaMetin.Text);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show(nesne.AdSoyadVer());
         }
     }
 }
